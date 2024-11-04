c956a2ebf7fcc8f3b75757424766cf174484101f
Beyerbach <beyerdan27@AD.7HILLS.ORG>
2024-11-04 09:30:49 -0500

    ASD

--- Board.java
+++ Board.java
@@ -1,3 +1,14 @@
 //board.java
 //5 ships, 1 of length 5, 1 of length 4, 2 of leangth 3, one of length 2
 //
+public class Board{
+	
+	
+	
+	public Board(/*params*/){
+		int ships[][] = new int[10][10];
+		int guess[][] = new int[10][10];
+		
+	}
+}
+//OUR LORD AND SAVIOUR, ARRAYLIST<>    
